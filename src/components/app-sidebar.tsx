e41c9467d0988e9baa0bb0f27f78d1ad60bc69dc
MlNl-PEKKA <punithg360@gmail.com>
2026-06-18 03:14:27 +0530

    side bar menu-items

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as React from "react";
 
 import {
@@ -16,173 +14,8 @@ import {
   SidebarRail,
 } from "~/components/ui/sidebar";
 import Image from "next/image";
-
-// This is sample data.
-const data = {
-  navMain: [
-    {
-      title: "Introduction",
-      url: "#",
-      items: [
-        {
-          title: "Welcome",
-          url: "#",
-        },
-        {
-          title: "Goals",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Installation",
-      url: "#",
-      items: [
-        {
-          title: "Rom Patcher JS",
-          url: "#",
-        },
-        {
-          title: "Hack Dex",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Features",
-      url: "#",
-      items: [
-        {
-          title: "Permanent Repel",
-          url: "#",
-        },
-        {
-          title: "Running",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Level Cap",
-          url: "#",
-        },
-        {
-          title: "Better Summary",
-          url: "#",
-        },
-        {
-          title: "Physical-Special split",
-          url: "#",
-        },
-        {
-          title: "Pocket Tutor",
-          url: "#",
-        },
-        {
-          title: "Better Evolutions",
-          url: "#",
-        },
-        {
-          title: "Adopt Eggs",
-          url: "#",
-        },
-        {
-          title: "Pocket PC",
-          url: "#",
-        },
-        {
-          title: "Pocket Heal",
-          url: "#",
-        },
-        {
-          title: "Forget HMs",
-          url: "#",
-        },
-        {
-          title: "Infinite TMs",
-          url: "#",
-        },
-        {
-          title: "Pocket Mart",
-          url: "#",
-        },
-        {
-          title: "1$ Items",
-          url: "#",
-        },
-        {
-          title: "Pocket Bikes",
-          url: "#",
-        },
-        {
-          title: "Auto Blend",
-          url: "#",
-        },
-        {
-          title: "Custom Blend",
-          url: "#",
-        },
-        {
-          title: "Badge Boost",
-          url: "#",
-        },
-        {
-          title: "EV Training",
-          url: "#",
-        },
-        {
-          title: "Catch Rate",
-          url: "#",
-        },
-        {
-          title: "Fast Eggs",
-          url: "#",
-        },
-        {
-          title: "Instant Fishing",
-          url: "#",
-        },
-        {
-          title: "Always Feebas",
-          url: "#",
-        },
-        {
-          title: "Better Safari",
-          url: "#",
-        },
-        {
-          title: "No Fleeing Mons",
-          url: "#",
-        },
-        {
-          title: "Instant Text",
-          url: "#",
-        },
-        {
-          title: "Music",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Credits",
-      url: "#",
-      items: [
-        {
-          title: "pret/pokeemrald",
-          url: "#",
-        },
-        {
-          title: "Code",
-          url: "#",
-        },
-        {
-          title: "Ideas",
-          url: "#",
-        },
-      ],
-    },
-  ],
-};
+import { menuItems } from "~/lib/menu-items";
+import Link from "next/link";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -191,21 +24,15 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <a href="#">
+              <Link href="/">
                 <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg border-2">
-                  <Image
-                    className="size-8"
-                    src="/logo.png"
-                    alt="Logo"
-                    width={32}
-                    height={32}
-                  />
+                  <Image src="/logo.png" alt="Logo" width={32} height={32} />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-medium">Pokemon Emerald Flow</span>
                   <span className="">v1.0.0-beta</span>
                 </div>
-              </a>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
@@ -213,19 +40,19 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarContent>
         <SidebarGroup>
           <SidebarMenu>
-            {data.navMain.map((item) => (
+            {menuItems.map((item) => (
               <SidebarMenuItem key={item.title}>
                 <SidebarMenuButton asChild>
-                  <a href={item.url} className="font-medium">
+                  <Link href={item.url} className="font-medium">
                     {item.title}
-                  </a>
+                  </Link>
                 </SidebarMenuButton>
                 {item.items?.length ? (
                   <SidebarMenuSub>
                     {item.items.map((item) => (
                       <SidebarMenuSubItem key={item.title}>
-                        <SidebarMenuSubButton asChild isActive={item.isActive}>
-                          <a href={item.url}>{item.title}</a>
+                        <SidebarMenuSubButton asChild isActive={false}>
+                          <Link href={item.url}>{item.title}</Link>
                         </SidebarMenuSubButton>
                       </SidebarMenuSubItem>
                     ))}
--- src/lib/menu-items.ts
+++ src/lib/menu-items.ts
@@ -1,40 +1,98 @@
-type Item = {
-  title: string;
-  items: string[];
-};
-
-type Items = Record<string, string[]>;
-
-const items: Items = {
-  Introduction: ["Welcome", "Goals"],
-  Installation: ["Rom Patcher JS", "Hack Dex"],
-  Features: [
-    "Permanent Repel",
-    "Running",
-    "Level Cap",
-    "Better Summary",
-    "Physical-Special split",
-    "Pocket Tutor",
-    "Better Evolutions",
-    "Adopt Eggs",
-    "Pocket PC",
-    "Pocket Heal",
-    "Forget HMs",
-    "Infinite TMs",
-    "Pocket Mart",
-    "1$ Items",
-    "Pocket Bikes",
-    "Auto Blend",
-    "Custom Blend",
-    "Badge Boost",
-    "EV Training",
-    "Catch Rate",
-    "Fast Eggs",
-    "Instant Fishing",
-    "Always Feebas",
-    "Better Safari",
-    "No Fleeing Mons",
-    "Instant Text",
-    "Music",
+const separatorIn = " " as const;
+const separatorOut = "-" as const;
+const urlPrefix = "#" as const;
+const sections = [
+  ["Introduction", ["Welcome", "Goals"]],
+  ["Installation", ["Rom Patcher JS", "Hack Dex"]],
+  [
+    "Features",
+    [
+      "Permanent Repel",
+      "Running",
+      "Level Cap",
+      "Better Summary",
+      "Physical-Special split",
+      "Pocket Tutor",
+      "Better Evolutions",
+      "Adopt Eggs",
+      "Pocket PC",
+      "Pocket Heal",
+      "Forget HMs",
+      "Infinite TMs",
+      "Pocket Mart",
+      "1$ Items",
+      "Pocket Bikes",
+      "Auto Blend",
+      "Custom Blend",
+      "Badge Boost",
+      "EV Training",
+      "Catch Rate",
+      "Fast Eggs",
+      "Instant Fishing",
+      "Always Feebas",
+      "Better Safari",
+      "No Fleeing Mons",
+      "Instant Text",
+      "Music",
+    ],
   ],
+  ["Credits", ["pret-pokeemerald", "Code", "Ideas"]],
+] as const satisfies Sections;
+
+const getUrl = <T extends string>(str: T) =>
+  `${urlPrefix}${str.toLowerCase().replaceAll(separatorIn, separatorOut)}` as `${typeof urlPrefix}${KebabCase<T>}`;
+
+const getMenuItems = <T extends Sections>(sections: T) =>
+  sections.map(([section, subSections]) => ({
+    title: section,
+    url: getUrl(section),
+    items: subSections.map((subSection) => ({
+      title: subSection,
+      url: getUrl(subSection),
+    })),
+  })) as MenuData<T>;
+
+export const menuItems = getMenuItems(sections);
+
+type Prettify<T> = {
+  [k in keyof T]: T[k];
+} & {};
+
+type Section = [string, string[]];
+
+type Sections = Section[];
+
+type KebabCase<
+  T extends string,
+  U extends string = "",
+> = T extends `${infer R extends string}${infer Rest}`
+  ? R extends typeof separatorIn
+    ? KebabCase<Rest, `${U}${typeof separatorOut}`>
+    : KebabCase<Rest, `${U}${Lowercase<R>}`>
+  : U;
+
+type MenuItemContent<T extends string = string> = {
+  title: T;
+  url: `${typeof urlPrefix}${KebabCase<T>}`;
 };
+
+type MenuSubItems<
+  T extends string[] = string[],
+  U extends MenuItemContent[] = [],
+> = T extends [infer R extends string, ...infer Rest extends string[]]
+  ? MenuSubItems<Rest, [...U, Prettify<MenuItemContent<R>>]>
+  : U;
+
+type MenuItem<T extends Section, U extends unknown[] = []> = T extends [
+  infer R extends string,
+  infer S extends string[],
+]
+  ? [...U, Prettify<MenuItemContent<R> & { items: MenuSubItems<S> }>]
+  : U;
+
+type MenuData<T extends Sections, U extends unknown[] = []> = T extends [
+  infer R extends Section,
+  ...infer S extends Sections,
+]
+  ? MenuData<S, [...U, ...MenuItem<R>]>
+  : U;
