25daa012e29b87b8558a3b4d5d29d6e59b23caf2
officer-kd6-3dot7 <punithg360@gmail.com>
2026-06-30 00:42:59 +0530

    layout height, theme default

--- src/app/(menu)/layout.tsx
+++ src/app/(menu)/layout.tsx
@@ -3,7 +3,7 @@ import { PageWrapper } from "~/components/page-wrapper";
 
 export default function Layout(props: PropsWithChildren) {
   return (
-    <PageWrapper className="mt-7 md:mt-17 md:min-h-[calc(100dvh-12rem)]">
+    <PageWrapper className="mt-7 md:mt-17 md:min-h-[calc(100dvh-13rem)]">
       {props.children}
     </PageWrapper>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,7 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="dark"
           disableTransitionOnChange
+          enableSystem
         >
           <TooltipProvider>
             <SidebarProvider>
